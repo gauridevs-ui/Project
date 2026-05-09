1d95e530dc193bae0b1e1544a00dc0b589b502ff
Gauri Chauhan <159995917+gauridevs-ui@users.noreply.github.com>
2026-05-09 03:38:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,10 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 
-app.config['MAIL_USERNAME'] = 'gaurichauhan411@gmail.com'
-app.config['MAIL_PASSWORD'] = 'uamjcxofrcgjkewj'
+import os
+
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 mail = Mail(app)
 
